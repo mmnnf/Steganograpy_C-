a4a7835f3d2ece3012574a877a96cca158ed48e3
nergizz <memmedzadenergizw@gmail.com>
2023-03-30 13:40:17 +0400

    deysiklik

---
+++ Steganography/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Steganography
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(34, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(196, 186);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Conteyner sec";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(259, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "gizledilecek datani sec";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(259, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "gizlet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(259, 150);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "datani cixart";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(259, 184);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(143, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "datani saxla";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(583, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 402);
+            this.textBox1.TabIndex = 6;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(259, 246);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(164, 139);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(834, 21);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(225, 400);
+            this.textBox2.TabIndex = 8;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(451, 96);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 94);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(259, 217);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(143, 23);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "stegonu yukle";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 435);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Steganography/Form1.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+
+
+
+namespace Steganography
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string dosyayolu = "";
+        Bitmap bmp;
+        string gizledilecek_data = "";
+        int [] mm;
+        int [] nn;
+        static String evvel = "";
+        static String sonra = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            dosyayolu = openFileDialog1.FileName;
+            bmp = new Bitmap(dosyayolu);
+            pictureBox1.Image = bmp;
+             byte[] mmb = takergba(bmp);
+             mm = new int[mmb.Length];
+             for (int i = 0; i < mm.Length; i++)
+             {
+                 mm[i] = mmb[i];
+             }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "jpeg files(*.jpg)|*.jpg";
+            if (DialogResult.OK == sfd.ShowDialog())
+            {
+                this.pictureBox2.Image.Save(sfd.FileName);
+            }
+        }
+       string gizledilecekdata_binnary = "";
+       string txt1 = "";
+       Bitmap bmp2;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            dosyayolu = openFileDialog1.FileName;
+            string fileType = Path.GetExtension(dosyayolu);
+            if (fileType==".txt")
+            {
+                StreamReader read = new StreamReader(File.OpenRead(dosyayolu));
+                gizledilecek_data = read.ReadToEnd();
+                textBox1.Text = gizledilecek_data;
+                read.Dispose();
+                int[] nn = new int[gizledilecek_data.Length];
+                for (int i = 0; i < gizledilecek_data.Length; i++)
+                {
+                    nn[i]=(int)gizledilecek_data[i];
+                }
+            }
+            else if (fileType == ".jpg")
+            {
+                  bmp2 = new Bitmap(dosyayolu);
+                pictureBox1.Image = bmp;
+                //byte [] nnb=takergba(bmp); //@@@@@@ @@@@@@   @@@@@@ @@@@@@  @2@@@
+                byte []nnb = takergba(bmp2);
+                nn = new int[nnb.Length];
+                for (int i = 0; i < nn.Length; i++)
+                {
+                    nn[i] = nnb[i];
+                } 
+            } 
+        }
+        static byte[] oobyte;
+        private void button3_Click(object sender, EventArgs e)
+        { 
+            int[] oo;
+            if ((bmp2.Width)*bmp2.Height*8+4>bmp.Height*bmp.Width)
+            {
+                MessageBox.Show("yerlesmir");
+            }
+            else
+            {
+                Stopwatch ww = new Stopwatch();
+                ww.Start();
+               
+            oo = tamSTEGOimageLIST (mm, nn, bmp2.Width, bmp2.Height);
+            ww.Stop();
+
+            oobyte = new byte[oo.Length];
+            int g = 0;
+            for (int i = 0; i < oo.Length; i++)
+            {
+                oobyte[i] = Convert.ToByte(oo[i]);
+                if (oobyte[i]!=oo[i])
+                {
+                    MessageBox.Show("alinmir");   
+                }
+            }
+            
+            Bitmap resim = bmp;
+            Bitmap yeniResim = new Bitmap(resim.Width, resim.Height, PixelFormat.Format24bppRgb);
+
+            int dizisayisi;
+            IntPtr baslangicy;  
+            Rectangle rcty;
+            BitmapData bmdatay;
+            rcty = new Rectangle(0, 0, resim.Width, resim.Height);
+            bmdatay = yeniResim.LockBits(rcty, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            baslangicy = bmdatay.Scan0;
+            dizisayisi = oobyte.Length;
+            Marshal.Copy(oobyte, 0, baslangicy, dizisayisi);
+            yeniResim.UnlockBits(bmdatay);
+            pictureBox2.Image = yeniResim;
+            MessageBox.Show("gizledildi");
+                Bitmap kk=new Bitmap(yeniResim);
+                ee = takergba(kk);
+                ee = oobyte;
+
+            }
+        }
+        static byte[] ee;
+        //////////////////////////////////////////////////////////////////
+        public static Byte[] takergba(Bitmap resim)
+        { 
+            int dizisayisi;
+            IntPtr baslangic;
+            byte[] rgbdeger;
+            Rectangle rct;
+            BitmapData bmdata;
+            rct = new Rectangle(0, 0, resim.Width, resim.Height);
+            bmdata = resim.LockBits(rct, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            baslangic = bmdata.Scan0;
+            dizisayisi = bmdata.Stride * resim.Height;
+            rgbdeger = new byte[dizisayisi];
+            Marshal.Copy(baslangic, rgbdeger, 0, dizisayisi);
+            resim.UnlockBits(bmdata);
+            //pictureBox1.Image = yeniResim;
+            return rgbdeger; 
+        }
+        public static StringBuilder ff = new StringBuilder();
+         
+        public static int[] tamTERSSTEGO_LIST(int[] mm)
+        {
+            int[] orginal = { };
+            List<int> tersmetabinarystm = new List<int>();
+            List<int> tersmetabinarystn = new List<int>();
+            for (int i = 0; i < 16; i++)
+            {
+                tersmetabinarystm.Add(mm[i] % 2);
+            }
+
+            //int tersmetaintm = Convert.ToInt32(tersmetabinarystm.ToString(), 2);
+            int tersmetaintm = binnary_to_decimal(tersmetabinarystm);
+            for (int i = 16; i < 32; i++)
+            {
+                tersmetabinarystn.Add(mm[i] % 2);
+            }  
+            int tersmetaintn = binnary_to_decimal(tersmetabinarystn); 
+
+            List<int> tersbinary_STB = new List<int>();
+            string tersbinary = "";
+            //MessageBox.Show(tersmetaintm+"--||--"+tersmetaintn);
+            //MessageBox.Show(tersmetaintm * tersmetaintn * 8 * 4 + ">>>>>>>>>>>>>>>>>" + mm.Length);
+            int size = tersmetaintm * tersmetaintn * 8 * 3;
+            for (int i = 32; i < 32 + tersmetaintm * tersmetaintn * 8 * 3; i++)
+            {
+
+                tersbinary_STB.Add(mm[i] % 2);
+
+            }
+            ///////////########BURA NIYE ISLEMIR
+            // sonra = tersbinary_STB.ToString();
+            List<int> ededstr_STB = new List<int>();
+            orginal = new int[((tersbinary_STB.Count) / 8) + 2];
+            for (int i = 0; i < tersbinary_STB.Count; i++)
+            {
+                ededstr_STB.Add(tersbinary_STB[i]);
+                if ((i + 1) % 8 == 0)
+                {
+                   // orginal[i / 8] = Convert.ToInt32(ededstr_STB.ToString(), 2);
+                    orginal[i / 8] = binnary_to_decimal(ededstr_STB);
+                    ededstr_STB.Clear();
+                }
+            }
+            //MessageBox.Show(tersmetaintm + "))))" + tersmetaintn);
+            orginal[orginal.Length - 2] = tersmetaintm;
+            orginal[orginal.Length - 1] = tersmetaintn;
+            return orginal;
+
+        }
+
+
+
+        //static StringBuilder sb;
+        //static StringBuilder sb2;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            int[] pp;
+            byte[] r = takergba(new Bitmap(pictureBox2.Image));  //stego seklin byte acilisi
+            //byte[] v = takergba(new Bitmap(pictureBox1.Image));   //ana seklin stego acilisi
+            int[] yy=new int [r.Length];
+            for (int i = 0; i < yy.Length; i++)
+            {
+                yy[i] = r[i];
+                //yy[i] = oobyte[i];
+                //yy[i] = ee[i];
+            }
+                 //sb=new StringBuilder();
+                 //sb2=new StringBuilder();
+            //int f = 0; 
+            //for (int i = 0; i < r.Length; i++)           //qapanisa geden stego sekil(oobyte) ile acilisa gelen stego seklin muqaisesi(r)
+            //{
+            //                          ///----MARAQLI NIYE ALINMIR
+            //   if (r[i] == oobyte[i])
+            //    {
+            //        f++;
+            //    }
+            //   //sb.Append(oobyte[i]+" ");
+            //   //sb2.Append(r[i]+" ");
+            //}
+            //if (f==r.Length)
+            //{
+            //    MessageBox.Show("heyyoooo");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("nooooo"+f);
+            //}
+
+
+            //File.WriteAllText(@"D:\oobyte.txt", sb.ToString());
+            //File.WriteAllText(@"D:\r.txt", sb2.ToString());
+            pp = tamTERSSTEGO_LIST(yy);
+            byte[] ppbyte = new byte[pp.Length-2];
+
+            for (int i = 0; i < pp.Length-2; i++)
+            {
+                ppbyte[i] = Convert.ToByte(pp[i]);
+            }
+            ////////ASAGINI METODA SALMAQ LAZIMDIR:
+             
+            Bitmap yeniResim = new Bitmap(pp[pp.Length - 2], pp[pp.Length - 1], PixelFormat.Format24bppRgb);
+
+            int dizisayisi;
+            IntPtr baslangicy;
+            Rectangle rcty;
+            BitmapData bmdatay;
+            rcty = new Rectangle(0, 0, pp[pp.Length-2] ,pp[pp.Length-1]);
+            bmdatay = yeniResim.LockBits(rcty, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            baslangicy = bmdatay.Scan0;
+            dizisayisi = ppbyte.Length;
+            Marshal.Copy(ppbyte, 0, baslangicy, dizisayisi);
+            yeniResim.UnlockBits(bmdatay);
+            pictureBox2.Image = yeniResim;
+
+        } 
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public static List<int> convert_number_tobinary(int n)
+        {
+            List<int> aa = new List<int>();
+            List<int> bb = new List<int>();
+            //aa.Clear();
+            //bb.Clear();
+            for (int i = 0; n > 0; i++)
+            {
+                aa.Add(n % 2);
+                n = n / 2;
+            }
+            for (int i = 0; i < aa.Count; i++)
+            {
+                bb.Add(aa[aa.Count - 1 - i]);
+            }
+            return bb;
+        }
+        public static List<int> convert_tobinary_N_lik(List<int> a, int n)
+        {
+            List<int> zeros = new List<int>(); 
+            int s = a.Count;
+            while (s < n)
+            {
+                zeros.Add(0);
+                s++;
+            }
+            zeros.AddRange(a);
+            return zeros;
+        }
+        public static List<int> convert_tobinary_tam(int n, int size)
+        {
+            return convert_tobinary_N_lik(convert_number_tobinary(n), size);
+        }
+        public static List<int> cevirikiliyeListt(int[] nn, int maxdigits)
+        { 
+            List<int> binarytaml = new List<int>();
+            List<int> binarytaml8llik = new List<int>();
+            for (int i = 0; i < nn.Length; i++)
+            {
+                binarytaml.AddRange(convert_tobinary_tam(nn[i], maxdigits));
+            }
+            return binarytaml;
+        }
+         public static int binnary_to_decimal(List<int> mm)
+        { 
+            int decimalValue = 0; 
+            int base1 = 1;
+            int s = mm.Count-1;
+            while (s >=0)
+            {
+                int reminder = mm[s];
+                decimalValue += reminder * base1;
+                base1 = base1 * 2;
+                s--;
+            }
+            return decimalValue;
+        }
+         public static int[] tamSTEGOimageLIST(int[] mm, int[] nn, int m, int n)
+         {
+
+             int[] cc = { m };
+             int[] dd = { n }; 
+             List<int> sm = cevirikiliyeListt(cc, 16);
+             List<int> sn = cevirikiliyeListt(dd, 16); 
+             List<int> binary32metadata = new List<int>();
+             binary32metadata.AddRange(sm);
+             binary32metadata.AddRange(sn);
+             mm = stegoSTB(binary32metadata, 0, mm); 
+             List<int> binarytam;   
+             if (nn.Length * 8 + 32 <= mm.Length)
+             {
+                 binarytam = cevirikiliyeListt(nn, 8);
+                 mm = stegoSTB(binarytam, 32, mm); 
+             }
+
+             else
+             {
+                 Console.WriteLine("massivin uzunlugu catmir");
+             } 
+             return mm;
+         }
+         public static int[] stegoSTB(List<int> binarytam, int idx, int[] mm)
+         {
+             for (int i = 0; i < binarytam.Count; i++)
+             {
+
+                 if (binarytam[i] == 0)
+                 {
+
+                     if (mm[i + idx] % 2 == 1)
+                     {
+                         mm[i + idx] -= 1;
+                     } 
+                 }
+                 else
+                 {
+                     if (mm[i + idx] % 2 != 1)
+                     {
+                         mm[i + idx] += 1;
+                     } 
+                 } 
+             } 
+             return mm;
+         }  
+
+         private void button9_Click(object sender, EventArgs e)
+         {
+             openFileDialog1.ShowDialog();
+             dosyayolu = openFileDialog1.FileName;
+             bmp = new Bitmap(dosyayolu);
+             pictureBox2.Image = bmp;
+             
+         }
+    }
+}
